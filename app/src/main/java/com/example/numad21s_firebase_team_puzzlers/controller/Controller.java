fe78af66a18a3998a5db73d05182da5537fca57a
Gustavo Amaral <amaral.g@husky.neu.edu>
2021-03-22 11:52:48 -0400

    create message model, controller class

---
+++ app/src/main/java/com/example/numad21s_firebase_team_puzzlers/controller/Controller.java
@@ -0,0 +1,21 @@
+package com.example.numad21s_firebase_team_puzzlers.controller;
+
+import com.example.numad21s_firebase_team_puzzlers.model.User;
+
+import java.util.List;
+
+public class Controller {
+    public boolean TryGetAllUsers(List<User> users) {
+        return false;
+    }
+
+    public boolean TryGetAllMessagesBetween(User user1, User user2) {
+        return false;
+    }
+
+    public boolean TryCreateNewUser() {
+        return false;
+
+    }
+
+}
---
+++ app/src/main/java/com/example/numad21s_firebase_team_puzzlers/model/Message.java
@@ -0,0 +1,27 @@
+package com.example.numad21s_firebase_team_puzzlers.model;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+import java.io.Serializable;
+import java.sql.Time;
+
+@IgnoreExtraProperties
+public class Message implements Serializable {
+    // TODO: store Time here, or use firebase's time feature?
+    // public Time time
+    public User userFrom;
+    public User userTo;
+    public int emojiID;
+
+    public Message(){
+        userFrom = null;
+        userTo  = null;
+        emojiID = -1;
+    }
+
+    public Message( User userFrom,  User userTo, int emojiID){
+        this.userFrom = userFrom;
+        this.userTo = userTo;
+        this.emojiID = emojiID;
+    }
+}
